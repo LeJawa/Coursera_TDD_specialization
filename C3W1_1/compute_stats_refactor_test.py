cbcfb44fba62376cd58511c18b6d3c50079d65dc
Luis Escobar Sawa <luis.escobar@mailbox.org>
2023-04-04 16:15:17 +0200

    Added more tests as part of C3W2 lab.

--- C3W1_1/compute_stats_refactor_test.py
+++ C3W1_1/compute_stats_refactor_test.py
@@ -1,4 +1,4 @@
-from C3W1_1.compute_stats_refactor import harmonic_mean, variance, standard_dev
+from C3W1_1.compute_stats_refactor import harmonic_mean, variance, standard_dev, count, average, minimum, maximum, summation
 import unittest
 
 class TestComputeStats(unittest.TestCase):
@@ -11,3 +11,19 @@ class TestComputeStats(unittest.TestCase):
 
     def test_standard_dev(self):
         self.assertAlmostEqual(standard_dev(), 285.44087653313)
+
+    def test_count(self):
+        self.assertEqual(1000, count())
+
+    def test_average(self):
+        self.assertAlmostEqual(499.498, average())
+
+    def test_summation(self):
+        self.assertEqual(499498, summation())
+
+    def test_minimum(self):
+        self.assertEqual(1, minimum())
+
+    def test_maximum(self):
+        self.assertEqual(997, maximum())
+
